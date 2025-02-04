3cbcaa69bc89a2ea97b625893301c1858148fd38
Elamparithi <126467358+Elamparithi06@users.noreply.github.com>
2025-02-04 18:40:02 +0530

    Update dropdownData.jsx

--- src/dropdownData.jsx
+++ src/dropdownData.jsx
@@ -1,7 +1,7 @@
 const dropdownData = {
     vsoOptions: ['VSO1', 'VSO2', 'VSO3', 'VSO4', 'VSO5', 'VSO6', 'VSO7', 'VSO8', 'VSO9', 'VSO10'],
     distributorOptions: [{'code': '0001', 'name': 'Distributor 01'}, {'code': '0002', 'name': 'Distributor 02'}, {'code': '0003', 'name': 'Distributor 03'}, {'code': '0004', 'name': 'Distributor 04'}, {'code': '0005', 'name': 'Distributor 05'}, {'code': '0006', 'name': 'Distributor 06'}, {'code': '0007', 'name': 'Distributor 07'}, {'code': '0008', 'name': 'Distributor 08'}, {'code': '0009', 'name': 'Distributor 09'}, {'code': '00010', 'name': 'Distributor 10'} ],
-    proceduralRemarks: ['Remark 1.', 'Remark 2', 'Remark 3','Remark 4','Remark 5','Remark 6','Remark 7''Remark 8''Remark 9','Remark 10']
+    proceduralRemarks: ['Remark 1.', 'Remark 2', 'Remark 3','Remark 4','Remark 5','Remark 6','Remark 7','Remark 8','Remark 9','Remark 10']
   };
   
   export default dropdownData;
